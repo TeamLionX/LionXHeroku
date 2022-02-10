84bc2d81a067c74b55b50b4caf5a6f2cc977e4da
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-02-10 09:11:24 +0530

    Update Lion.py

--- Lion.py
+++ Lion.py
@@ -6,8 +6,7 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://github.com/TeamLionX/LionX && cd LionX && python3 -m userbot")
+system("git clone https://github.com/TeamLionX/Lion && cd LionX && python3 -m userbot")
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
 # Script written for stop heroku suspension
-# use with credit else gay
